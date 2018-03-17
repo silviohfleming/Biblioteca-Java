ed41cb48e7759c07c23af9adf7153f820d04fe40
Silvio Henrique Fleming <silviohfleming@gmail.com>
2018-03-17 17:02:08 -0300

    feat(autorTest): Add autorTest methods

--- src/main/java/br/biblioteca/livros/repository/AutorRepository.java
+++ src/main/java/br/biblioteca/livros/repository/AutorRepository.java
@@ -3,4 +3,6 @@ package br.biblioteca.livros.repository;
 import br.biblioteca.livros.beans.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AutorRepository extends JpaRepository<Autor, Long> { }
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Autor findByNome(String nome);
+}
---
+++ src/test/java/br/biblioteca/livros/beans/AutorTest.java
@@ -0,0 +1,74 @@
+package br.biblioteca.livros.beans;
+
+import br.biblioteca.livros.repository.AutorRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutorTest {
+
+    private Autor autor;
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Before
+    public void criaAutor() {
+        this.autor = new Autor();
+    }
+
+    @Test
+    public void buscarTodosOsAutoresCadastrados() {
+
+        Page<Autor> autores = this.autorRepository.findAll(new PageRequest(0, 10));
+        assertThat(autores.getTotalElements()).isGreaterThan(1L);
+
+    }
+
+    @Test
+    public void buscarAutorPeloNome() {
+
+        Autor autorNaoEncontrado = this.autorRepository.findByNome("Carlos Drummond de Andrada");
+        assertThat(autorNaoEncontrado).isNull();
+
+        Autor autorEncontrado = this.autorRepository.findByNome("Ariano Suassuna");
+        assertThat(autorEncontrado).isNotNull();
+        assertThat(autorEncontrado.getNome()).isEqualTo("Ariano Suassuna");
+
+    }
+
+    @Test
+    public void deveAdicionarNovoAutor()
+    {
+        criaAutor();
+        autor.setId(4L);
+        autor.setNome("Papito Love");
+        autorRepository.save(autor);
+    }
+
+    @Test
+    public void deveEditarAutorExistente()
+    {
+        autor = autorRepository.findByNome("Ariano Suassuna");
+        autor.setNome("Jorge da Kombi");
+        autorRepository.save(autor);
+    }
+
+    @Test
+    public void deveExcluirAutorPeloNome()
+    {
+//        autor = autorRepository.findByNome("Ariano Suassuna");
+//        autorRepository.delete(autor);
+//        assertThat(autor).isNull();
+    }
+
+}
